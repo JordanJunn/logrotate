1291a10915373b23b2002f5cec01333b1f2e1179
Junn <jmacey@assetnote.io>
2022-01-24 11:59:36 +1000

    fix(logrotate): use host env vars in cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 service cron start
 # Docker Entrypoint, generates logrotate config file, sets up crontab and hand over to `tini`
 gomplate < logrotate.tpl.conf > /etc/logrotate.conf
-
-echo "$CRON_SCHEDULE /usr/sbin/logrotate -v /etc/logrotate.conf > /var/log/logrotate.log 2>&1" | crontab -
+# grab host envars
+export >> /etc/profile.d/env.sh 
+echo "$CRON_SCHEDULE /bin/bash -l -c '/usr/sbin/logrotate -v /etc/logrotate.conf > /var/log/logrotate.log 2>&1'" | crontab -
 
 touch /var/log/logrotate.log
 tail -f /var/log/logrotate.log
